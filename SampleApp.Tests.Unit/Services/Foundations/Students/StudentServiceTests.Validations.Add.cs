ee4f0fc38f4453ea591aa859147923e62b39998b
Andrew McClelland <andrew.mcclelland@microsoft.com>
2022-10-03 16:52:01 -0400

    ShouldThrowValidationExceptionOnAddIfNullAndLogItAsync -> FAIL

--- SampleApp.Tests.Unit/Services/Foundations/Students/StudentServiceTests.Validations.Add.cs
+++ SampleApp.Tests.Unit/Services/Foundations/Students/StudentServiceTests.Validations.Add.cs
@@ -1,6 +1,47 @@
-﻿namespace SampleApp.Tests.Unit.Services.Foundations.Students
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using SampleApp.Models.Students;
+using SampleApp.Models.Students.Exceptions;
+using Xunit;
+
+namespace SampleApp.Tests.Unit.Services.Foundations.Students
 {
     public partial class StudentServiceTests
     {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnAddIfNullAndLogItAsync()
+        {
+            // given
+            Student nullStudent = null;
+            var nullStudentException = new NullStudentException();
+
+            var expectedStudentValidationException =
+                new StudentValidationException(nullStudentException);
+
+            // when
+            ValueTask<Student> addStudentTask =
+                this.studentService.AddStudentAsync(nullStudent);
+
+            StudentValidationException actualStudentValidationException =
+                await Assert.ThrowsAsync<StudentValidationException>(
+                    addStudentTask.AsTask);
+
+            // then
+            actualStudentValidationException
+                .Should().BeEquivalentTo(expectedStudentValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(
+                    SameExceptionAs(expectedStudentValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertStudentAsync(It.IsAny<Student>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
