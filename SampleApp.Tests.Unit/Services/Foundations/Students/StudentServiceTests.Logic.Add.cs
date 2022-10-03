65d699953340a76f8de4c9e85e9091e0f15299ba
Hassan Habib <hassanhabib@live.com>
2022-10-03 13:19:24 -0700

    ShouldAddStudentAsync -> FAIL

--- SampleApp.Tests.Unit/Services/Foundations/Students/StudentServiceTests.Logic.Add.cs
+++ SampleApp.Tests.Unit/Services/Foundations/Students/StudentServiceTests.Logic.Add.cs
@@ -1,6 +1,41 @@
-﻿namespace SampleApp.Tests.Unit.Services.Foundations.Students
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using SampleApp.Models.Students;
+using Xunit;
+
+namespace SampleApp.Tests.Unit.Services.Foundations.Students
 {
     public partial class StudentServiceTests
     {
+        [Fact]
+        public async Task ShouldAddStudentAsync()
+        {
+            // given
+            Student randomStudent = CreateRandomStudent();
+            Student inputStudent = randomStudent;
+            Student insertedStudent = inputStudent;
+            Student expectedStudent = insertedStudent.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertStudentAsync(inputStudent))
+                    .ReturnsAsync(insertedStudent);
+
+            // when
+            Student actualStudent =
+                await this.studentService.AddStudentAsync(
+                    inputStudent);
+
+            // then
+            actualStudent.Should().BeEquivalentTo(expectedStudent);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertStudentAsync(inputStudent),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
